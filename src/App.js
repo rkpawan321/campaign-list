1d75f68ad411723f625c6ad1a1382494d00a55aa
Pawan RK <pawan@clinikk.com>
2021-02-20 11:50:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CampaignPage from './components/Campaigns';
+
+function App() {
+  return (
+    <div>
+      <CampaignPage />
+    </div>
+  );
+}
+
+export default App;
